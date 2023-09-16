9a5bae6e7ab1b674a6d229467608656e835760d3
shuyu liu <liushuyu6666@gmail.com>
2023-09-15 22:46:22 -0400

    rename src/myworkdayjobs/MyWorkDayJobs.ts to src/myworkdayjobs/WorkDayJobs.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import { mongodbConnection } from './src/mongodb';
-import { MyWorkDayJobs } from './src/myworkdayjobs/MyWorkDayJobs';
+import { MyWorkDayJobs } from './src/myworkdayjobs/WorkDayJobs';
 
 async function main() {
     const connection = await mongodbConnection('AutoJobs');
-    console.log(typeof MyWorkDayJobs);
     const myWorkDayJobs = new MyWorkDayJobs(
         'src/myworkdayjobs/myworkdayjobs.json',
     );
